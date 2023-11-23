e55eaf9d0a63b06adb178b69491a553d8e89634d
avideb24 <debavi24@gmail.com>
2023-11-23 23:26:49 +0600

    setup project with react, react router, tailwind, dailsyUI

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+
+    </>
+  )
+}
+
+export default App
